953604c523030a331f5759cb0a3e5b3d099b492f
Kaitlyn Friden <kfriden@verizon.net>
2021-06-12 01:39:23 -0400

    added sign in and sign up pages and implemented redux forms

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import SignInForm from './signinForm';
 
 class SignIn extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
     render() {
         return (
             <div className="sign-in">
-                sign inn
+                <SignInForm onSubmit={this.onSubmit} className="sign-in__form" />
             </div>
         )
     }
