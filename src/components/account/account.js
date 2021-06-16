6037974b1a4ac55a1b7c9387de1b9c6d952205a8
Kaitlyn Friden <kfriden@verizon.net>
2021-06-15 21:44:45 -0400

    finished actions for the header and added more links

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -8,6 +8,19 @@ class Account extends Component {
 
     componentDidMount() {
 
+        const headerLinks = [ 
+            {
+                _id: 0,
+                title: "Shop",
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
+
         const navbarLinks = [
             {
                 _id: 0,
@@ -22,7 +35,7 @@ class Account extends Component {
                 component: <AccountInfo />
             }
         ]
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
 
