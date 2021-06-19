68aa14203ba8ca4fbe605688c4f854c57e57b882
Kaitlyn Friden <kfriden@verizon.net>
2021-06-19 00:11:26 -0400

    added purchase detail component

--- src/actions/user.js
+++ src/actions/user.js
@@ -18,7 +18,7 @@ export function fetchUserPurchases() {
                 _id: 0,
                 total: 8.02,
                 orderNumber: 'A0048248343',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Jordan Hudgens',
@@ -30,7 +30,7 @@ export function fetchUserPurchases() {
                 _id: 1,
                 total: 50.99,
                 orderNumber: 'A55UNHG80F',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Luise Cachorro',
@@ -42,7 +42,7 @@ export function fetchUserPurchases() {
                 _id: 2,
                 total: 10.14,
                 orderNumber: 'P990CDF211',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Homer Simpson',
@@ -54,7 +54,7 @@ export function fetchUserPurchases() {
                 _id: 3,
                 total: 42.00,
                 orderNumber: 'HU3376FR$',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Benjamin Tellie',
@@ -66,7 +66,7 @@ export function fetchUserPurchases() {
                 _id: 4,
                 total: 13.17,
                 orderNumber: '7UUCVF890D',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Maddie Belchamp',
@@ -78,7 +78,7 @@ export function fetchUserPurchases() {
                 _id: 5,
                 total: 10.70,
                 orderNumber: 'T00CFM5189',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Kit Borden',
@@ -90,7 +90,7 @@ export function fetchUserPurchases() {
                 _id: 6,
                 total: 72.35,
                 orderNumber: 'VEN00231SW',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Laura Bard',
@@ -102,7 +102,7 @@ export function fetchUserPurchases() {
                 _id: 7,
                 total: 23.50,
                 orderNumber: 'A07GGYOP3',
-                orderDate: new Date(),
+                orderDate: new Date().toDateString(),
                 creditCard: '-0000',
                 user: {
                     name: 'Masie Francis',
