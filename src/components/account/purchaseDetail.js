9eb77b90e9eb2e5a9a8902bde44c82ff1b1ebe66
Kaitlyn Friden <kfriden@verizon.net>
2021-06-20 23:38:57 -0400

    fixed a bug with all purchases being the same

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 function PurchaseDetailLabel({className, title, value}) {
     return (
         <div className={`${className} purchase-detail-label`}>
-            <div className="purchase-detail-label__label">{title}</div>
+            <div className="purchase-detail-label__title">{title}</div>
             <div className='purchase-detail-label__value'>{value}</div>
         </div>
     )
@@ -19,9 +19,11 @@ class PurchaseDetail extends Component {
             <div className={`&{className} purchase-detail`}>
                 <PurchaseDetailLabel className='purchase-detail__order-number' title='Order Number' value={orderNumber} />
                 <PurchaseDetailLabel className='purchase-detail__order-date' title='Order Date' value={orderDate} />
-                <PurchaseDetailLabel className='purchase-detail__shipping' title='Shipping Address' value={`${name}\n${shippingAddress}`} />
+                <PurchaseDetailLabel className='purchase-detail__shipping-address' title='Shipping Address' value={`${name}\n${shippingAddress}`} />
                 <PurchaseDetailLabel className='purchase-detail__total' title='Total' value={total} />
                 <PurchaseDetailLabel className='purchase-detail__credit-card' title='Credit Card' value={creditCard} />
+                <a className='purchase-detail__track-shipment'>Track Shipment</a>
+                <a className='purchase-detail__print-receipt'>Print Receipt</a>
             </div>
         )
         
