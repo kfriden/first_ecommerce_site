624f9d52aa37fdce5858e45a1538ec090043b700
Kaitlyn Friden <kfriden@verizon.net>
2021-06-24 23:34:56 -0400

    used headernavbar to create new shop navbar and updated action/reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import headerNavbar from './headernavbarReducer';
 import user from './userReducer';
+import shop from './shopReducer';
 
 const rootReducer = combineReducers({
-  form, headerNavbar, user
+  form, headerNavbar, user, shop
 });
 
 export default rootReducer;
